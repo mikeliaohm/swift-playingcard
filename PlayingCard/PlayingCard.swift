fc90a13bc1855087cbc9588f99b73e81e5198a53
Mike Liao (mac mini) <mike.hm.liao@gmail.com>
2019-03-19 23:43:54 +0800

    first commit

---
+++ PlayingCard/PlayingCard.swift
@@ -0,0 +1,72 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by Hsin-Min Liao on 2019/3/19.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible
+{
+    var description: String { return "\(rank)\(suit)" }
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .spades: return Suit.spades.rawValue
+            case .hearts: return Suit.hearts.rawValue
+            case .diamonds: return Suit.diamonds.rawValue
+            case .clubs: return Suit.clubs.rawValue
+            }
+        }
+        
+        case spades = "♠️"
+        case hearts = "♥️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        static var all = [Suit.spades, .hearts, .diamonds, .clubs]
+    }
+    
+    enum Rank: CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return "\(pips)"
+            case .face(let kind) where kind == "J": return "J"
+            case .face(let kind) where kind == "Q": return "Q"
+            case .face(let kind) where kind == "K": return "K"
+            default: return "0"
+            }
+        }
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks: [Rank] = [.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), face("Q"), face("K")]
+            return allRanks
+        }
+    }
+}
