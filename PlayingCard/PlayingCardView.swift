bb72d9b2c46da680e74c46178838e1ece374d3a7
Mike Liao (mac mini) <mike.hm.liao@gmail.com>
2019-03-20 23:44:37 +0800

    sample drawing of a circle (use UIBezierPath)

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,31 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Hsin-Min Liao on 2019/3/20.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        if let context = UIGraphicsGetCurrentContext() {
+            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+            context.setLineWidth(5.0)
+            UIColor.green.setFill()
+            UIColor.red.setStroke()
+            context.strokePath() // path is consumed so fillPath of next line has no effect
+            context.fillPath()
+        }
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
+    }
+
+}
